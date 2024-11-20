9b264a2cea27448a8ad1b74006be3f6c9905d9b9
Yujin <yj999900@gmail.com>
2024-11-20 15:48:52 +0900

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavermapsProvider } from 'react-naver-maps';
+import { Routes, Route, Link } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import MyMap from './components/MapComponent';
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavermapsProvider ncpClientId='yz53tx4y0m'>
+      <div style={{ margin: '20px' }}>
+        <nav>
+          {/* 네비게이션 메뉴 */}
+          <ul>
+            <li>
+              <Link to='/'>홈</Link>
+            </li>
+            <li>
+              <Link to='/map'>지도</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* 라우팅 설정 */}
+        <Suspense fallback={<div>로딩 중...</div>}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/map' element={<MyMap />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </NavermapsProvider>
   );
 }
 
 export default App;
+
