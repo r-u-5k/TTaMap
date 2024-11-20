65ca58026304ea215b2bd24599846c7f0ebe96eb
Yujin <yj999900@gmail.com>
2024-11-20 15:59:22 +0900

    .

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -5,8 +5,8 @@ function MyMap() {
   return (
     <MapDiv style={{ width: '100%', height: '600px' }}>
       <NaverMap
-        defaultCenter={new navermaps.LatLng(37.5665, 126.9780)} // 서울의 위도와 경도
-        defaultZoom={10} // 줌 레벨
+        defaultCenter={new navermaps.LatLng(37.540797, 127.076583)} // 위도, 경도
+        defaultZoom={16} // 줌 레벨
       />
     </MapDiv>
   );
