a7ab146078e04ba014974b3a51e22cb46d02362b
Yujin <yj999900@gmail.com>
2024-11-28 21:58:50 +0900

    도보 이동 거리, 시간 추출 성공

--- routes/services.py
+++ routes/services.py
@@ -2,10 +2,10 @@ import requests
 import urllib.parse
 import params as pa
 
-def fetch_public_transport_route():
+def get_public_transport_route(departure_lat, departure_lon, arrival_lat, arrival_lon):
     api_key = pa.ODSAY_API_KEY
     encoded_api_key = urllib.parse.quote(api_key, encoding='utf-8')
-    url_info = f"https://api.odsay.com/v1/api/searchPubTransPathT?SX=126.9027279&SY=37.5349277&EX=126.9145430&EY=37.5499421&apiKey={encoded_api_key}"
+    url_info = f"https://api.odsay.com/v1/api/searchPubTransPathT?SY={departure_lat}&SX={departure_lon}&EY={arrival_lat}&EX={arrival_lon}&apiKey={encoded_api_key}"
 
     response = requests.get(url_info, headers={"Content-type": "application/json"})
 
--- routes/urls.py
+++ routes/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('pt', views.public_transport_route_view, name="public_transport_route"),
+]
