25b5e2552e0ead59332301c9df34696a4ec72e36
lunegyu <mins3577@gmail.com>
2024-12-05 16:53:42 +0900

    react

--- routes/views.py
+++ routes/views.py
@@ -11,11 +11,11 @@ def pt_route_view(request):
     try:
         data = get_odsay_route(start_lat, start_lng, end_lat, end_lng)
         if data['result']['path'][0]['subPath'][0]['trafficType'] == 3:
-            print(f'출발지에서 대중교통 탑승 전까지 도보 이동 거리: {data['result']['path'][0]['subPath'][0]['distance']}m')
-            print(f'출발지에서 대중교통 탑승 전까지 도보 이동 시간: {data['result']['path'][0]['subPath'][0]['sectionTime']}분')
+            print(f"출발지에서 대중교통 탑승 전까지 도보 이동 거리: {data['result']['path'][0]['subPath'][0]['distance']}m")
+            print(f"출발지에서 대중교통 탑승 전까지 도보 이동 시간: {data['result']['path'][0]['subPath'][0]['sectionTime']}분")
         if data['result']['path'][0]['subPath'][-1]['trafficType'] == 3:
-            print(f'대중교통 하차 후 목적지까지 도보 이동 거리: {data['result']['path'][0]['subPath'][-1]['distance']}m')
-            print(f'대중교통 하차 후 목적지까지 도보 이동 시간: {data['result']['path'][0]['subPath'][-1]['sectionTime']}분')
+            print(f"대중교통 하차 후 목적지까지 도보 이동 거리: {data['result']['path'][0]['subPath'][-1]['distance']}m")
+            print(f"대중교통 하차 후 목적지까지 도보 이동 시간: {data['result']['path'][0]['subPath'][-1]['sectionTime']}분")
 
         return JsonResponse(data)
     except Exception as e:
